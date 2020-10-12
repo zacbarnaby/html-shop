7d0bc7ed8da4f75c8ec8111e5922ea3c18ff99fa
Zac Barnaby <zac.barnaby@hotmail.co.uk>
2020-10-12 21:12:34 +0100

    Index finished

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,19 +2,26 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', () => {
     return gulp.src('./src/assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/assets/css'));
+				.pipe(gulp.dest('./public/assets/css'))
+				.pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', () => {
-    gulp.watch('./src/assets/scss/**/*.scss', (done) => {
-        gulp.series(['sass'])(done);
-    });
-});
+gulp.task('serve', gulp.series('sass', () => {
+	browserSync.init({
+		server: {
+			baseDir: './public'
+		}
+	});
 
-gulp.task('default', gulp.series(['sass:watch']));
+	gulp.watch('./src/assets/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('./public/*.html').on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('serve'));
